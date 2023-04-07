cab9d115588d75cbdefaf06d25f0c596084e0e98
weare4saken <kondrashka91@gmail.com>
2023-04-07 18:25:31 +0700

    Initial commit

---
+++ src/main/java/com/skypro/simpleaviasales/SimpleAviasalesApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.simpleaviasales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAviasalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAviasalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/simpleaviasales/SimpleAviasalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.simpleaviasales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAviasalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
